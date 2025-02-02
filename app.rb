add0e5f6124c2b64799309435540daf9d985b268
sycalvind <114632354+sycalvind@users.noreply.github.com>
2025-02-02 17:10:43 +0000

    finished

--- app.rb
+++ app.rb
@@ -9,57 +9,46 @@ get("/:function/new") do
   @function = params.fetch("function")
   function_sym = @function.to_sym
   erb(function_sym)
-  # add form to each function's erb
 end
 
 get("/square/results") do
-  # logic for calculations
-  @value = params.fetch("number").to_f
+  @function = "square"
+  @function_print = "square"
+  @number = params.fetch("number")
+  @value = @number.to_f
   @result = @value ** 2
-  @print_hash = {"Number"=>@value, "Square"=>@result}
+  @print_hash = {"Number"=>@number, "Square"=>@result}
   erb(:result)
-  # store array for things to print
-  # erb result
 end
 
 get("/square_root/results") do
-  # logic for calculations
-  # store array for things to print
-  # erb result
-end
-
-get("/square_root/results") do
-  # logic for calculations
-  # store array for things to print
-  # erb result
+  @function = "square_root"
+  @function_print = "square root"
+  number = params.fetch("user_number")
+  result = Math.sqrt(number.to_f)
+  @print_hash = {"Number"=>number, "Square Root"=>result}
+  erb(:result)
 end
 
 get("/payment/results") do
-  # logic for calculations
-  # store array for things to print
-  # erb result
+  @function = "payment"
+  @function_print = "payment"
+  apr = params.fetch("user_apr").to_f
+  years = params.fetch("user_years").to_i
+  pv = params.fetch("user_pv").to_f
+  r = (apr/100) / 12
+  n = years * 12
+  p = (r * pv)/(1 - (1+r)**(-n))
+  @print_hash = {"APR"=>apr.to_fs(:percentage, {:precision=>4}), "Number of years"=>years, "Principal"=>pv.to_fs(:currency), "Payment"=>p.to_fs(:currency)}
+  erb(:result)
 end
 
 get("/random/results") do
-  # logic for calculations
-  # store array for things to print
-  # erb result
+  @function = "random"
+  @function_print = "random"
+  min = params.fetch("user_min").to_f
+  max = params.fetch("user_max").to_f
+  result = rand(min..max)
+  @print_hash = {"Minimum"=>min, "Maximum"=>max, "Square Root"=>result}
+  erb(:result)
 end
-
-=begin
-get("/square_root/new") do
-  erb(:square_root)
-end
-
-get("/square/new") do
-  erb(:square)
-end
-
-get("/payment/new") do
-  erb(:payment)
-end
-
-get("/payment/new") do
-  erb(:payment)
-end
-=end
